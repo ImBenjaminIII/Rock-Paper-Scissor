61a3ca7e6ed02c248ebc3782d0299e8ce1e98ad6
Benjamin <angelmoreno.am205@gmail.com>
2023-06-13 22:16:22 +0800

    adding javascript logic & adding ui

--- script.js
+++ script.js
@@ -1,119 +1,163 @@
-// Buttons
-const rockBtn = document.querySelector('.rockBtn');
-const paperBtn = document.querySelector('.paperBtn');
-const scissorBtn = document.querySelector('.scissorBtn');
+const btns = document.querySelectorAll('#allButtons .buttons');
+let playerSign = document.querySelector('#player .sign');
+const playerScore = document.querySelector('#player .score');
+const computerScore = document.querySelector('#computer .score');
+let computerSign = document.querySelector('#computer .sign');
+let roundMessage = document.querySelector('#round-message');
+let resultWinner = document.querySelector('#winner');
+let gamelogResult = document.querySelector('#game-log .result');
 
-let options = ['Rock', 'Paper', 'Scissor'];
-let permissionToExit;
-let roundWin = '';
+// options to select, computer choice.
+const options = ['Rock', 'Paper', 'Scissor'];
 let userScore = 0;
 let comScore = 0;
+let computerChoice = '';
+let playerChoice = '';
+let gameOver = false;
 
-// return a random computer choice
+btns.forEach(buttons => {
+  buttons.addEventListener('click', () => {
+    if (buttons.textContent === 'ROCK') {
+      computerChoice = getComputerChoice();
+      playerChoice = getFormatPlayerChoice(buttons.textContent);
+    } else if (buttons.textContent === 'SCISSOR') {
+      computerChoice = getComputerChoice();
+      playerChoice = getFormatPlayerChoice(buttons.textContent);
+    } else if (buttons.textContent === 'PAPER') {
+      computerChoice = getComputerChoice();
+      playerChoice = getFormatPlayerChoice(buttons.textContent);
+    }
+
+    handleChoice(playerChoice, computerChoice);
+  });
+});
+
+// return a random computer choice.
 function getComputerChoice() {
   let randomChoice = options[Math.floor(Math.random() * 3)];
   return randomChoice;
 }
 
-// return a player choice
-function getPlayerChoice() {
-  while (true) {
-    let playerChoice = prompt('Choose weapon: [Rock, Paper, Scissor]');
-
-    if (playerChoice != null) {
-      playerChoice =
-        playerChoice.charAt(0).toUpperCase() +
-        playerChoice.slice(1).toLowerCase();
-
-      if (options.includes(playerChoice)) {
-        return playerChoice;
-      } else if (playerChoice === null) {
-        return permissionExit();
-      } else {
-        continue;
-      }
-    } else {
-      return permissionExit();
-    }
-  }
+// return a format player choice.
+function getFormatPlayerChoice(choice) {
+  let playerChoice =
+    choice.charAt(0).toUpperCase() + choice.slice(1).toLowerCase();
+  return playerChoice;
 }
 
-// ask user to confirm cancelation in picking.
-function permissionExit() {
-  let grant = confirm('Are you sure? You want to cancel');
-  if (grant) {
-    permissionToExit = 'You cancel, successfully.🥹';
-    return permissionToExit;
-  } else {
-    getPlayerChoice();
+function handleChoice(playerChoice, computerChoice) {
+  if (checkWinner() != false) {
+    console.log('Hello');
+    return;
   }
+
+  switch (playerChoice) {
+    case 'Rock':
+      playerSign.textContent = '✊';
+      break;
+    case 'Paper':
+      playerSign.textContent = '🖐️';
+      break;
+    case 'Scissor':
+      playerSign.textContent = '✌️';
+  }
+
+  switch (computerChoice) {
+    case 'Rock':
+      computerSign.textContent = '✊';
+      break;
+    case 'Paper':
+      computerSign.textContent = '🖐️';
+      break;
+    case 'Scissor':
+      computerSign.textContent = '✌️';
+  }
+
+  playRound(playerChoice, computerChoice);
 }
 
 // function game play round
 function playRound(userChoice, comChoice) {
+  let roundWin;
+
   if (userChoice === comChoice) {
-    console.log("It's a draw! ");
+    roundMessage.textContent = "It's a draw!";
+    roundMessage.style.color = 'gray';
   } else if (userChoice === 'Rock') {
     if (comChoice === 'Scissor') {
-      roundWin = 'USER';
-      console.log('You win!');
+      roundWin = 'player';
+      roundMessage.textContent = 'You Win!';
+      roundMessage.style.color = 'green';
     } else if (comChoice === 'Paper') {
-      roundWin = 'COMPUTER';
-      console.log('You lose!');
+      roundWin = 'computer';
+      roundMessage.textContent = 'You Lose!';
+      roundMessage.style.color = 'red';
     }
   } else if (userChoice === 'Paper') {
     if (comChoice === 'Rock') {
-      roundWin = 'USER';
-      console.log('You win!');
+      roundWin = 'player';
+      roundMessage.textContent = 'You Win!';
+      roundMessage.style.color = 'green';
     } else if (comChoice === 'Scissor') {
-      roundWin = 'COMPUTER';
-      console.log('You lose!');
+      roundWin = 'computer';
+      roundMessage.textContent = 'You Lose!';
+      roundMessage.style.color = 'red';
     }
   } else if (userChoice === 'Scissor') {
     if (comChoice === 'Paper') {
-      roundWin = 'USER';
-      console.log('You win!');
+      roundWin = 'player';
+      roundMessage.textContent = 'You Win!';
+      roundMessage.style.color = 'green';
     } else if (comChoice === 'Rock') {
-      roundWin = 'COMPUTER';
-      console.log('You lose!');
+      roundWin = 'computer';
+      roundMessage.textContent = 'You Lose!';
+      roundMessage.style.color = 'red';
     }
   }
 
-  if (permissionToExit != 'You cancel, successfully.🥹') {
-    console.log(addScore(roundWin));
-  }
-
-  return isGameOver();
+  addScore(roundWin);
+  gameLog(roundWin);
 }
 
+// display game log
+function gameLog(winner) {
+  let newGamelogResult = document.createElement('p');
+  if (winner != undefined) {
+    if (winner === 'player') {
+      newGamelogResult.textContent = `The ${winner} new score : ${userScore}`;
+      newGamelogResult.style.color = 'green';
+    } else if (winner === 'computer') {
+      newGamelogResult.textContent = `The ${winner} new score : ${comScore}`;
+      newGamelogResult.style.color = 'red';
+    }
+  } else {
+    newGamelogResult.textContent = `DRAW`;
+  }
+  gamelogResult.appendChild(newGamelogResult);
+}
 // add score and return a string of score.
 function addScore(roundWinner) {
-  if (roundWinner === 'USER') {
+  if (roundWinner == 'player') {
     userScore++;
-  } else if (roundWinner === 'COMPUTER') {
+  } else if (roundWinner == 'computer') {
     comScore++;
   }
 
-  return `👨🏻 Score : ${userScore} || 🤖 Score : ${comScore}`;
+  playerScore.textContent = userScore;
+  computerScore.textContent = comScore;
+
+  // check winner
+  checkWinner();
 }
 
-// return a winner
-function isGameOver() {
+function checkWinner() {
   if (userScore === 5) {
-    return '🎉🎉You Win the battle!!🎉🎉';
+    resultWinner.textContent = 'Player Won!🥳';
+    return true;
   } else if (comScore === 5) {
-    return '😢😢Computer Win the battle!!🥲🥲';
-  } else if (permissionToExit === 'You cancel, successfully.🥹') {
-    //if permission to exit is true return the execution!
-    return 'You cancel❌';
+    resultWinner.textContent = 'Computer Won!🥹';
+    return true;
+  } else {
+    return false;
   }
-}
-
-function game() {
-  while (!isGameOver()) {
-    playRound(getPlayerChoice(), getComputerChoice());
-  }
-
-  // Return the winner
-  return isGameOver();
 }
